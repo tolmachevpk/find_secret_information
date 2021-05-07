5a239288381c7c9f0f5e64039c78a4ddf57d0309
Petr Tolmachev <tolmachev.pk@phystech.edu>
2021-05-07 04:40:32 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/find_secret_information.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find_secret_information.dir/main.cpp.o"
+  "find_secret_information"
+  "find_secret_information.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find_secret_information.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
